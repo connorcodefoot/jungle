ec6b49202fa6b979d33230d1a2b670caadf6de16
Connor <cgbroadfoot@gmail.com>
2023-03-08 11:38:12 +0000

    Add basic authentication to dashboard so that it can only be accessed by auth user

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,3 +2,7 @@ class Admin::DashboardController < ApplicationController
   def show
   end
 end
+
+class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'] 
+end
