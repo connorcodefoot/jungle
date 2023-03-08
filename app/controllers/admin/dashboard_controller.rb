041bad4350f57dd0db846e2da374938bf77b30d0
Connor <cgbroadfoot@gmail.com>
2023-03-08 13:06:33 +0000

    Create instance variables that correspond to db queries for getting number of products and number of categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,10 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'] 
 
   def show
-  end
-
-  def product_count
-    @product_count = Product.all.count
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
 end
