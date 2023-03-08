5f40958c8acd6213cd0b284f3bc664017dace91b
Connor <cgbroadfoot@gmail.com>
2023-03-08 11:38:47 +0000

    Add basic auth so only auth user can manage products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -38,3 +38,7 @@ class Admin::ProductsController < ApplicationController
   end
 
 end
+
+class ApplicationController < ActionController::Base
+  http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'] 
+end
