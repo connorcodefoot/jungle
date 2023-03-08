f7a9141924686b6a7bcf9185f740ef091eb926ac
Connor <cgbroadfoot@gmail.com>
2023-03-08 14:54:11 +0000

    Fix syntax issue with routes, it is now working as desired

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/new'
+    get 'categories/create'
+  end
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
