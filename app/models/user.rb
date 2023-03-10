9100753c4de844b7612854300a111409f0082552
Connor <cgbroadfoot@gmail.com>
2023-03-10 05:05:39 +0000

    Add validation to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, confirmation: true, unless: -> { password.blank? }
+
+                        
+  validates :password_confirmation, presence: true
+
+
 end
