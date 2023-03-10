1a38b2e4f58d70eaa80bbc3818c58af787960327
Connor <cgbroadfoot@gmail.com>
2023-03-10 06:31:41 +0000

    Add new method to models class that authenticates user upon login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 
+  def self.authenticate_with_credentials(email, password)
+    
+    user = User.find_by_email(email)
+    raise "Invalid email or password" if user.nil? or not user.authenticate password
+
+    return user
+  end
+
   validates :name, 
     presence: true
   validates :email, 
